8b259d2b63a896054901c2ae8e44656c026e3581
Rafael <98551688+etzoider@users.noreply.github.com>
2023-02-18 11:33:46 -0300

    Style done

--- script.js
+++ script.js
@@ -1,3 +1,112 @@
 
 
-//         <img src="./img/woman-looking-up-eyes-closed.jpg" alt="Side-Image">
+
+function submitForm(form) {
+
+    alert(form);
+    
+}
+
+
+
+/*
+
+
+
+
+<div class="header-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.
+            
+            <div class="header-last-sentence">Your journey begins here.</div>
+        </div>
+
+        <form action="return submitForm(form)" class="form">
+
+           <div class="line">
+
+            <span>
+
+
+                <label for="firstName" id="firstName">First name
+                    <input type="text" placeholder="name">
+                </label>
+            </span>
+            
+            
+            
+            <span>
+                <label for="lastName" id="lastName">Last name
+                    <input type="text" placeholder="lastName">
+                </label>
+    
+              </span>
+    
+           </div>
+
+
+
+    <div class="line">
+
+        <span>
+        <label for="email" id="email">Email
+            <input type="email" placeholder="email">
+        </label>
+</span>
+
+
+<span>
+        <label for="phone" id="phone">Phone
+            <input type="number" placeholder="phone">
+        </label>
+
+
+
+    </span>
+
+    </div>
+
+
+<div class="line">
+
+    <span>
+        <label for="password" id="password">Password
+            <input type="password" placeholder="password">
+        </label>
+
+    </span>
+    
+    
+    <span>
+        <label for="confirmPassword" id="confirmPassword">Confirm password
+            <input type="password" placeholder="passwordCONFIRM">
+        </label>
+
+    </span>
+
+
+
+
+</div>
+
+                <label for="receiveEmail" id="receiveEmail">Would you like to be part of our newsletter?
+                    <input type="checkbox" checked>
+                </label>
+
+
+
+                <button type="submit">Submit</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
